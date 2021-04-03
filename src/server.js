031bacf38c35e5349579d1c92b5a75500f1a071f
José Moreira <canasvieiras@hotmail.com.br>
2021-04-03 10:52:21 -0300

    3/04

---
+++ src/server.js
@@ -0,0 +1,10 @@
+
+const express = require("express")
+const server = express()
+
+server.get('/', (request, response) => {
+    console.log('entrei no index')
+    return response.send('oi')
+})
+
+server.listen(3000, () => console.log('servidor rodando'))
