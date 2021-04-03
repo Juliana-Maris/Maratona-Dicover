83eca96bd6b142ab79569f332fed38fcfb3e53a5
José Moreira <canasvieiras@hotmail.com.br>
2021-04-03 18:08:57 -0300

    template engine

--- src/views/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routes = express.Router()
-
-routes.get('/', (request, response) => {
-    
-    return response.sendFile(__dirname + "/views/index.html")
-})
-
-module.exports = routes
