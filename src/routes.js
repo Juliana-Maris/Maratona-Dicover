7fb7a54d7c6a62c784ebe53d0771a84ef80f2563
José Moreira <canasvieiras@hotmail.com.br>
2021-04-08 18:54:28 -0300

    nova fatoração

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@ const express = require("express"); //cria biblioteca do servidor
 const routes = express.Router() //cria rotas
 const ProfileController = require('./controllers/ProfileController')
 const JobController = require('./controllers/JobController')
+const DashboardController = require('./controllers/DashboardController')
          
 
-routes.get('/', JobController.index)
+routes.get('/', DashboardController.index)
 routes.get('/job', JobController.create)
 routes.post('/job', JobController.save)  
 routes.get('/job/:id', JobController.show)
