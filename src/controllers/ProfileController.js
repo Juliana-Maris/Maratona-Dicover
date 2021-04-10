9d7e00d19ebc08da7bca4c14999bee1e07ff30a7
José Moreira <canasvieiras@hotmail.com.br>
2021-04-10 09:04:02 -0300

    profile atualizado

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,10 +1,10 @@
 const Profile = require('../model/Profile')
 
 module.exports = {
-    index(req, res) {
-     return res.render("profile", { profile: Profile.get() })
+    async index(req, res) {
+     return res.render("profile", { profile: await Profile.get() })
     },
-     update(req, res) {
+     async update(req, res) {
       // req. body para pegar os dados
       const data = req.body
       // definir quantas semanas tem num ano
@@ -18,9 +18,11 @@ module.exports = {
       // qual valor da hora?
       const valueHour = data["monthly-budget"] / monthlyTotalHours
         
+      const profile = await Profile.get()
+      
       // ... é espalhar os dados, pegar todos
-       Profile.update({  
-        ...Profile.get(),
+       await Profile.update({  
+        ... profile,
         ...req.body, 
         "value-hour": valueHour 
       }) 
--- src/model/Job.js
+++ src/model/Job.js
@@ -1,4 +1,5 @@
 
+
 let data = [
     { 
         id: 1,
